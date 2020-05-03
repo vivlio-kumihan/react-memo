bf8c7afad4e72434d2f49fb874ea97a4143898cf
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-05-03 12:40:07 +0900

    elementについて復習と写経をする。

--- react-guide/src/index.js
+++ react-guide/src/index.js
@@ -2,3 +2,88 @@
 import React, { useReducer, Component } from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
+
+// const name = "John"
+// const element = <h1>{name}</h1>
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById("root")
+// )
+
+
+// function formatName() {
+//   return (
+//     `${ name.first } ${ name.last }`
+//   )
+// }
+
+// const name = {
+//   first: "John",
+//   last: "Lennon"
+// }
+
+// const element = <h1>{formatName(name)}</h1>
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById("root")
+// )
+  
+  
+// function getGreet(name) {
+//   return (`Hello, ${name.first} ${name.last }`)
+// }
+  
+// const name = {
+//   first: "John",
+//   last: "Lennon"
+// }
+
+// const element = (
+//   <h1>{getGreet(name)}</h1>
+// )
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById("root")
+// )
+
+
+// function getGreet(name) {
+//   if (name) {
+//     return `${name.first} ${name.last }`
+//   } else {
+//     return "こんにちは。"
+//   }
+// }
+
+// const name = {
+//   first: "John",
+//   last: "Lennon"
+// }
+
+// const element = (
+//   // <h1>{getGreet(name)}</h1>
+//   <h1>{getGreet()}</h1>
+// )
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById("root")
+// )
+
+
+// function tick() {
+//   const element = (
+//   <h1>Now, {new Date().toLocaleTimeString()}</h1>
+//   )
+
+//   ReactDOM.render(
+//     element,
+//     document.getElementById("root")
+//   )
+// }
+
+// setInterval(tick, 1000)
+
