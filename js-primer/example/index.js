011a53588e35765708b96db0e78009e3e4874d88
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-06-15 17:22:27 +0900

    learn javascript

---
+++ js-primer/example/index.js
@@ -0,0 +1,34 @@
+"use strict"
+
+arr = ["nob", "kazu", "mari"];
+[a, b] = arr;
+console.log(a);
+
+// console.log(1 == "1")
+// console.log(1 === "1")
+
+
+// const name = {
+//   "first": "nob",
+//   "last": "takahiro"
+// };
+
+// console.log(name.first);
+// console.log(name.last);
+// console.log(`Hello, ${name.first} ${name.last}.`);
+// console.log(`Hello, ${name["first"]} ${name["last"]}.`);
+
+
+// const name = "nob";
+// console.log(`My name is ${name}!`);
+
+
+// console.log(typeof ture);
+// console.log(typeof 31);
+// console.log(typeof "takahiro");
+// console.log(typeof Symbol("シンボル"));
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof ["nob", "kazu", "mari"]);
+// console.log(typeof { "key": "value" });
+// console.log(typeof function() {});
